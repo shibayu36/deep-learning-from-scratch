a5a33e04fe27e08adc0469d0e078a0073d93b6b2
shibayu36 <shibayu36@gmail.com>
2023-05-04 22:07:28 +0900

    XOR

--- ch02/perceptron.py
+++ ch02/perceptron.py
@@ -56,3 +56,16 @@ print(OR(0, 0))  # 0
 print(OR(1, 0))  # 1
 print(OR(0, 1))  # 1
 print(OR(1, 1))  # 1
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    return AND(s1, s2)
+
+
+print("XOR")
+print(XOR(0, 0))  # 0
+print(XOR(1, 0))  # 1
+print(XOR(0, 1))  # 1
+print(XOR(1, 1))  # 0
