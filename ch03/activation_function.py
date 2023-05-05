9791093994c7e01a5574d85724fc880d94876e60
shibayu36 <shibayu36@gmail.com>
2023-05-05 14:00:17 +0900

    neuralnet by sample_weight.pkl

--- ch03/activation_function.py
+++ ch03/activation_function.py
@@ -21,9 +21,22 @@ def relu(x):
     return np.maximum(0, x)
 
 
-x = np.arange(-5.0, 5.0, 0.1)
-# y = step_function(x)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
-plt.show()
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)  # for overflow
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+def identity_function(x):
+    return x
+
+
+# x = np.arange(-5.0, 5.0, 0.1)
+# # y = step_function(x)
+# y = sigmoid(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
+# plt.show()
