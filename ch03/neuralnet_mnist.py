0f4cc4971d52269bb5499eb8e8adb78dce64e711
shibayu36 <shibayu36@gmail.com>
2023-05-05 14:08:03 +0900

    Batch

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import time
 import append_path as _
 import numpy as np
 from dataset.mnist import load_mnist
@@ -41,11 +42,29 @@ def predict(network, x):
 x, t = get_data()
 network = init_network()
 
+start = time.time()
 accuracy_cnt = 0
 for i in range(len(x)):
     y = predict(network, x[i])
     p = np.argmax(y)
     if p == t[i]:
         accuracy_cnt += 1
+finish = time.time()
 
 print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+print("Time: " + str(finish - start) + "s")
+
+start = time.time()
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i : i + batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i : i + batch_size])
+
+finish = time.time()
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+print("Time: " + str(finish - start) + "s")
