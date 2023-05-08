8134bf7b70afeb94c6a9df690624b390eca2d5f9
shibayu36 <shibayu36@gmail.com>
2023-05-08 23:13:37 +0900

    cross_entropy_error with batch

--- ch04/loss_function.py
+++ ch04/loss_function.py
@@ -6,5 +6,10 @@ def sum_squared_error(y, t):
 
 
 def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
     delta = 1e-7  # to handle when y == 0
-    return -np.sum(t * np.log(y + delta))
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + delta)) / batch_size
