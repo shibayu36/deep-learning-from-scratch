1d26dbb6061ba6d9b916529a0f2ba7cf04572809
shibayu36 <shibayu36@gmail.com>
2023-05-08 23:13:47 +0900

    numerical gradient

---
+++ ch04/numerical_gradient.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        # f(x+h)
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+
+def function_1(x):
+    return 0.01 * x**2 + 0.1 * x
+
+
+def function_2(x):
+    return x[0] ** 2 + x[1] ** 2
+
+
+# x = np.arange(0.0, 20.0, 0.1)
+# y = function_1(x)
+# plt.xlabel("x")
+# plt.ylabel("f(x)")
+# plt.plot(x, y)
+# plt.show()
+
+# print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+# print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+# print(numerical_gradient(function_2, np.array([3.0, 0.0])))
+
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
