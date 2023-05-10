19c6c6be5d4e506f13e91f487fb95f990f2f830a
shibayu36 <shibayu36@gmail.com>
2023-05-10 16:19:17 +0900

    Calculate accuracy

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -5,13 +5,17 @@ from two_layer_net_with_gradient import TwoLayerNet
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
-
 # hyper parameters
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+# The number of iterations per a epoch
+iter_per_epoch = max(train_size / batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -31,5 +35,13 @@ for i in range(iters_num):
     # Record the learning process
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    # Calculate accuracy every epoch
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
 
 print(train_loss_list)
