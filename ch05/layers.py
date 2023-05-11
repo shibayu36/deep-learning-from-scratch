27cfaeffaf0e7d8287ff5320a2948056c42ecfa4
shibayu36 <shibayu36@gmail.com>
2023-05-11 16:57:33 +0900

    Affine layer

--- ch05/layers.py
+++ ch05/layers.py
@@ -1,3 +1,4 @@
+import numpy as np
 from common.functions import sigmoid
 
 
@@ -42,5 +43,31 @@ class Sigmoid:
         dout: numpy array
         """
         dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
+class Affine:
+    """
+    Affine transformation layer for batch input
+    """
+
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
 
         return dx
