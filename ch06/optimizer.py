a91331cfcc1d7700d144376dff96d774195b6392
shibayu36 <shibayu36@gmail.com>
2023-05-12 15:10:26 +0900

    Momentum

--- ch06/optimizer.py
+++ ch06/optimizer.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class SDG:
     def __init__(self, lr=0.01):
         self.lr = lr
@@ -5,3 +8,22 @@ class SDG:
     def update(self, params, grads):
         for key in params.keys():
             params[key] -= self.lr * grads[key]
+
+
+class Momentum:
+    # 感覚的には、ボールが動くように、同じ方向に力がかかっているなら加速していくイメージ。
+    # これにより、緩やかな傾斜の場合でも、少しずつ大きく進むようになる
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params:
+                self.v[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
