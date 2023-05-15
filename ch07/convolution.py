340426c3624b23e4741ded2288fb4ecc0b6c78d4
shibayu36 <shibayu36@gmail.com>
2023-05-15 10:04:48 +0900

    Convolution filter

---
+++ ch07/convolution.py
@@ -0,0 +1,29 @@
+import append_path
+import numpy as np
+from common.util import im2col
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        """
+        W: weight, shape = (FN, C, FH, FW)
+        b: bias, shape = (FN, 1)
+        stride: stride, int
+        pad: padding, int
+        """
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2 * self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2 * self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
