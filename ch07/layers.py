588557e4fe997bd0f21ebc7365d69a99d5cc10e0
shibayu36 <shibayu36@gmail.com>
2023-05-17 09:40:46 +0900

    Pooling

--- ch07/layers.py
+++ ch07/layers.py
@@ -40,3 +40,45 @@ class Convolution:
         dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
 
         return dx
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=2, pad=0):
+        # if pool window is 2x2, pool_h = pool_w = 2, stride = 2 in general
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        """
+        x: numpy array, shape = (N, C, H, W)
+        """
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        # 1. expand
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        # 2. max
+        out = np.max(col, axis=1)
+
+        # 3. reshape
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+
+        return dx
